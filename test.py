11d984cc92ba414d22aa10b7640bbccfd9b84a2b
chris-tobar <60682082+chris-tobar@users.noreply.github.com>
2022-10-18 16:23:12 -0700

    Creation of TODOs

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-
+#TODO figure out how to get the proper html to show exactly how it does on the web browser
+#TODO figure out what framework is being used, in order to properly emulate the page being shown
 URL = "https://neetcode.io/practice"
 
 page = requests.get(URL)
